a3a24c02c6408f8abb917c8bfb4feef48f2d72f2
Olivia Trewin <zancoproductions@gmail.com>
2018-04-08 19:02:44 -0500

    Attempt to deduce the format

--- Alzaitu.BlackMagic.Playground/Program.cs
+++ Alzaitu.BlackMagic.Playground/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 
 namespace Alzaitu.BlackMagic.Playground
@@ -7,35 +9,88 @@ namespace Alzaitu.BlackMagic.Playground
     {
         public static void Main(string[] args)
         {
+            Jump();
             var test = new TestClass();
             var r = new FakeTypedReference(__makeref(test));
             var ptr = (IntPtr*)Marshal.ReadIntPtr(r.Value).ToPointer();
             ptr[0] = typeof(SuperClass).TypeHandle.Value;
             var s = new FakeTypedReference(r.Value, typeof(SuperClass)).GetValue<SuperClass>();
+
+            ((IntPtr**) typeof(SuperClass).TypeHandle.Value.ToPointer())[10][4] = typeof(SomeOtherClass)
+                .GetMethod(nameof(SomeOtherClass.SomeSortOfMethod)).MethodHandle.GetFunctionPointer();
+
             s.Test();
             test.Test();
+        }
+
+        public static void Jump()
+        {
+            var asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(Guid.NewGuid().ToString("N")),
+                AssemblyBuilderAccess.Run);
+            var module = asm.DefineDynamicModule("<Module>");
+            var type = module.DefineType(Guid.NewGuid().ToString("N"));
+            var method = type.DefineMethod(Guid.NewGuid().ToString("N"),
+                MethodAttributes.HideBySig | MethodAttributes.Public);
+            method.GetILGenerator().Emit(OpCodes.Ret);
+
+            var cType = type.CreateType();
         }
     }
 
     public class SuperClass
     {
-        public string var = "SOME STRING";
-        public int test = 0x1234;
+        public SuperClass()
+        {
+            Console.WriteLine(".ctor SuperClass");
+        }
 
         public virtual void Test()
         {
-            Console.WriteLine("No override");
+            Console.WriteLine(nameof(SuperClass));
+        }
+
+        public virtual void Final()
+        {
+            Console.WriteLine("Final SuperClass");
+        }
+
+        public override string ToString()
+        {
+            return "";
         }
     }
 
-    public class TestClass
+    public class TestClass : SuperClass
     {
-        public string var = "SOME STRING";
-        public int test = 0x1234;
-
-        public virtual void Test()
+        public TestClass()
         {
-            Console.WriteLine("Override");
+            Console.WriteLine(".ctor TestClass");
+        }
+
+        public override void Test()
+        {
+            Console.WriteLine(nameof(TestClass));
+        }
+    }
+
+    public class FinalClass : TestClass
+    {
+        public override void Test()
+        {
+            Console.WriteLine(nameof(FinalClass));
+        }
+
+        public override void Final()
+        {
+            Console.WriteLine("Final FinalClass");
+        }
+    }
+
+    public class SomeOtherClass
+    {
+        public static void SomeSortOfMethod(TestClass obj)
+        {
+            Console.WriteLine("I'm not even part of this class!");
         }
     }
 }                                  
---
+++ Alzaitu.BlackMagic/RawJump.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Alzaitu.BlackMagic
+{
+    public unsafe class RawJump
+    {
+        public static void Jump()
+        {
+            var asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(Guid.NewGuid().ToString("N")),
+                AssemblyBuilderAccess.Run);
+            var module = asm.DefineDynamicModule("<Module>");
+            var type = module.DefineType(Guid.NewGuid().ToString("N"));
+            var method = type.DefineMethod(Guid.NewGuid().ToString("N"),
+                MethodAttributes.HideBySig | MethodAttributes.Public);
+            method.GetILGenerator().Emit(OpCodes.Ret);
+
+            var cType = type.CreateType();
+        }
+    }
+}
+
