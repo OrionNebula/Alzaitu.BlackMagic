72d5cb0c8285fc375ceb9687841d807e243ad60e
Olivia Trewin <zancoproductions@gmail.com>
2018-04-09 21:01:26 -0500

    Cleanup

--- Alzaitu.BlackMagic.Playground/Program.cs
+++ Alzaitu.BlackMagic.Playground/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 
 namespace Alzaitu.BlackMagic.Playground
@@ -9,31 +7,17 @@ namespace Alzaitu.BlackMagic.Playground
     {
         public static void Main(string[] args)
         {
-            Jump();
             var test = new TestClass();
             var r = new FakeTypedReference(__makeref(test));
             var ptr = (IntPtr*)Marshal.ReadIntPtr(r.Value).ToPointer();
             ptr[0] = typeof(SuperClass).TypeHandle.Value;
             var s = new FakeTypedReference(r.Value, typeof(SuperClass)).GetValue<SuperClass>();
 
-            ((IntPtr**) typeof(SuperClass).TypeHandle.Value.ToPointer())[10][4] = typeof(SomeOtherClass)
-                .GetMethod(nameof(SomeOtherClass.SomeSortOfMethod)).MethodHandle.GetFunctionPointer();
+            /*((IntPtr**) typeof(SuperClass).TypeHandle.Value.ToPointer())[10][4] = typeof(SomeOtherClass)
+                .GetMethod(nameof(SomeOtherClass.SomeSortOfMethod)).MethodHandle.GetFunctionPointer();*/
 
             s.Test();
             test.Test();
-        }
-
-        public static void Jump()
-        {
-            var asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(Guid.NewGuid().ToString("N")),
-                AssemblyBuilderAccess.Run);
-            var module = asm.DefineDynamicModule("<Module>");
-            var type = module.DefineType(Guid.NewGuid().ToString("N"));
-            var method = type.DefineMethod(Guid.NewGuid().ToString("N"),
-                MethodAttributes.HideBySig | MethodAttributes.Public);
-            method.GetILGenerator().Emit(OpCodes.Ret);
-
-            var cType = type.CreateType();
         }
     }
 
@@ -60,14 +44,14 @@ namespace Alzaitu.BlackMagic.Playground
         }
     }
 
-    public class TestClass : SuperClass
+    public class TestClass
     {
         public TestClass()
         {
             Console.WriteLine(".ctor TestClass");
         }
 
-        public override void Test()
+        public virtual void Test()
         {
             Console.WriteLine(nameof(TestClass));
         }
@@ -80,7 +64,7 @@ namespace Alzaitu.BlackMagic.Playground
             Console.WriteLine(nameof(FinalClass));
         }
 
-        public override void Final()
+        public virtual void Final()
         {
             Console.WriteLine("Final FinalClass");
         }
